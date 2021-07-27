f2edf206177cae8b4c47c5cf0d055ae60f09895b
Amiteswary <72036734+Amiteswary@users.noreply.github.com>
2021-07-27 08:57:52 +0530

    Create Day16.py

---
+++ Day16.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+S = input().strip()
+try:
+        int_s=int(S)
+        print(int_s)
+except :
+        print("Bad String")
+    
+    
